6a3708b972f344aedd5a446b57a98e0d278e104c
Juliano Kassner <juliano.kassner@ciss.com.br>
2021-03-20 23:33:39 -0300

    Ajuste data formato errado

--- src/main/java/br/com/jkassner/popula_resultados/serviceImpl/CrawlerService.java
+++ src/main/java/br/com/jkassner/popula_resultados/serviceImpl/CrawlerService.java
@@ -1,11 +1,5 @@
 package br.com.jkassner.popula_resultados.serviceImpl;
 
-import java.time.LocalDate;
-import java.util.Date;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import br.com.jkassner.pojo.dto.ConcursoLotoFacilDto;
 import br.com.jkassner.pojo.dto.EnumDescricaoFaixa;
 import br.com.jkassner.pojo.dto.RateioDto;
@@ -13,6 +7,11 @@ import br.com.jkassner.pojo.model.Cidade;
 import br.com.jkassner.pojo.model.ConcursoLotoFacil;
 import br.com.jkassner.pojo.model.DezenasLotoFacilOrdenadas;
 import br.com.jkassner.popula_resultados.repository.lotofacil.ConcursoLotoFacilRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.ZoneId;
+import java.util.Date;
 
 @Service
 public class CrawlerService {
@@ -25,7 +24,7 @@ public class CrawlerService {
 		ConcursoLotoFacil concurso = new ConcursoLotoFacil();
 		
 		concurso.setIdConcurso(concursoDto.getNumero());
-		concurso.setDtSorteio(getDtSorteio(concursoDto.getDataApuracao()));
+		concurso.setDtSorteio(Date.from(concursoDto.getDataApuracao().atStartOfDay(ZoneId.systemDefault()).toInstant()));
 		concurso.setVlArrecadacaoTotal(concursoDto.getValorArrecadado());
 		concurso.setVlAcumuladoEspecial(concursoDto.getValorAcumuladoConcursoEspecial());
 		concurso.setVlAcumulado15Num(concursoDto.getValorAcumuladoProximoConcurso());
@@ -91,14 +90,6 @@ public class CrawlerService {
 		concurso.setDteDezena(concursoDto.getDezenasSorteadasOrdemSorteio().get(12));
 		concurso.setDqaDezena(concursoDto.getDezenasSorteadasOrdemSorteio().get(13));
 		concurso.setDqiDezena(concursoDto.getDezenasSorteadasOrdemSorteio().get(14));
-	}
-
-	private Date getDtSorteio(LocalDate localDate) {
-		int ano = localDate.getYear();
-		int mes = localDate.getMonthValue();
-		int dia = localDate.getDayOfMonth();
-		
-		return new Date(ano, mes, dia);
 	}
 	
 	private void setNumeroGanhadores(ConcursoLotoFacilDto concursoDto, ConcursoLotoFacil concursoLotoFacil) {
